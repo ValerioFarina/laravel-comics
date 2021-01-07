e12bbfb8402499dbca045517c3facd064a3805da
Valerio Farina <valerio.farina.1994@gmail.com>
2021-01-07 17:51:51 +0100

    Completatto menu dell'header bottom

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
 
         <title>@yield('page_title', 'DC Comics')</title>
 
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -18,6 +18,101 @@
     </div>
 
     <div id="header-bottom">
+        <div class="container">
+            <div class="d-flex justify-content-between">
+                <div class="logo">
+                    <img src="{{ asset('images/dc-logo.png') }}" alt="DC Comics Logo">
+                </div>
 
+                <div class="menu">
+                    <ul>
+                        <li>
+                            <a href="#" class="upper-case">
+                                characters
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#" class="upper-case">
+                                comics
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#" class="upper-case">
+                                movies
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#" class="upper-case">
+                                tv
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#" class="upper-case">
+                                games
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#" class="upper-case">
+                                collectibles
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#" class="upper-case">
+                                videos
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#" class="upper-case">
+                                fans
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#" class="upper-case">
+                                news
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#" class="upper-case">
+                                shop
+                                <span>▼</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="search">
+
+                </div>
+            </div>
+        </div>
     </div>
 </header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+FANS
+
+NEWS
+
+SHO
