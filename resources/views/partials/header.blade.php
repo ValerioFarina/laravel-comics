702c0bb02374f49e92fb8e124c1cbcdfa57c1149
Valerio Farina <valerio.farina.1994@gmail.com>
2021-01-07 17:15:55 +0100

    Stilato l'header top

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,23 @@
+<header>
+    <div id="header-top">
+        <div class="container">
+            <ul>
+                <li>
+                    <a href="#" class="upper-case">
+                        DC power℠ visa®
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#" class="upper-case">
+                        additional DC sites ▼
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div id="header-bottom">
+
+    </div>
+</header>
