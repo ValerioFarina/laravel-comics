88935221021dcb3b69c2f7df14ba02c6ca6c2ca8
Valerio Farina <valerio.farina.1994@gmail.com>
2021-01-08 18:16:08 +0100

    Creata la struttura della parte inferiore della sezione "comics-info"

--- resources/views/comics-details.blade.php
+++ resources/views/comics-details.blade.php
@@ -11,6 +11,12 @@
             <div class="small-container">
                 <div class="thumb">
                     <img src="{{ $single_comics['thumb'] }}" alt="">
+                    <span class="comics-type upper-case">
+                        {{ $single_comics['type'] }}
+                    </span>
+                    <span class="cta upper-case">
+                        view gallery
+                    </span>
                 </div>
             </div>
         </div>
@@ -54,7 +60,73 @@
         </div>
 
         <div class="bottom">
+            <div class="small-container clearfix">
+                <div class="talent float-left">
+                    <div class="row">
+                        <h3>
+                            Talent
+                        </h3>
+                    </div>
+                    <div class="row clearfix">
+                        <div class="col float-left">
+                            <h5>
+                                Art by:
+                            </h5>
+                        </div>
+                        <div class="col float-left">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </div>
+                    </div>
+                    <div class="row clearfix">
+                        <div class="col float-left">
+                            <h5>
+                                Written by:
+                            </h5>
+                        </div>
+                        <div class="col float-left">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </div>
+                    </div>
+                </div>
 
+                <div class="specs float-left">
+                    <div class="row">
+                        <h3>
+                            Specs
+                        </h3>
+                    </div>
+                    <div class="row clearfix">
+                        <div class="col float-left">
+                            <h5>
+                                Series:
+                            </h5>
+                        </div>
+                        <div class="col float-left">
+                            -------
+                        </div>
+                    </div>
+                    <div class="row clearfix">
+                        <div class="col float-left">
+                            <h5>
+                                U.S. Price:
+                            </h5>
+                        </div>
+                        <div class="col float-left">
+                            -------
+                        </div>
+                    </div>
+                    <div class="row clearfix">
+                        <div class="col float-left">
+                            <h5>
+                                On Sale Date:
+                            </h5>
+                        </div>
+                        <div class="col float-left">
+                            -------
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
