20ba85bf6a179453ede88fe941a7e4a89bdddaac
Valerio Farina <valerio.farina.1994@gmail.com>
2021-01-09 09:39:02 +0100

    Creata struttura della parte superiore del footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,164 @@
 <footer>
     <div id="footer-top">
         <div class="container">
+            <div id="footer-menus" class="clearfix">
+                <div class="col float-left">
+                    <div class="menu">
+                        <h3 class="upper-case">Title</h3>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
 
+                    <div class="menu">
+                        <h3 class="upper-case">Title</h3>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col float-left">
+                    <div class="menu">
+                        <h3 class="upper-case">Title</h3>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col float-left">
+                    <div class="menu">
+                        <h3 class="upper-case">Title</h3>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Link
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <p class="copyright">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
         </div>
     </div>
 
