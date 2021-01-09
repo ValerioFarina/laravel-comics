9d727f4c375595440a025f6ae0aa21baabd31aed
Valerio Farina <valerio.farina.1994@gmail.com>
2021-01-09 09:17:47 +0100

    Inserite le due immagini di sfondo nel footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,13 @@
+<footer>
+    <div id="footer-top">
+        <div class="container">
+
+        </div>
+    </div>
+
+    <div id="footer-bottom">
+        <div class="container">
+
+        </div>
+    </div>
+</footer>
